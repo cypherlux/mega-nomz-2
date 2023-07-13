e44c0a03c823393dc31b6f3ffadde5edecb8eaec
cypherlux <89822852+cypherlux@users.noreply.github.com>
2023-07-13 15:05:37 -0700

    create environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 import recipes from './data/recipes.js';
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 
 const app = express();
